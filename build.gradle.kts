1dd8b0bcde876cfcb3027bcb870eaf43dc20abaa
EinNxk <einnxk@gmail.com>
2026-08-10 22:34:40 +0200

    chore: Further initial setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,32 @@
 plugins {
     id("java")
+    id("java-library")
+    id("maven-publish")
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.spotless)
 }
 
-group = "de.einnik.build-plugin"
-version = "1.0-SNAPSHOT"
+group = "dev.einnik.chainy"
+version = "1.0.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+val targetVersion = 25
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(targetVersion))
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 dependencies {
+    api(rootProject.libs.lombok)
+    annotationProcessor(rootProject.libs.lombok)
+
+    api(rootProject.libs.jspecify)
+
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -17,4 +34,24 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    mergeServiceFiles()
+
+    exclude("META-INF/*.SF")
+    exclude("META-INF/*.DSA")
+    exclude("META-INF/*.RSA")
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar)
+}
+
+spotless {
+    java {
+        licenseHeaderFile(rootProject.file("config/license-header.txt"), "^(package|import|module) ")
+        googleJavaFormat()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,22 @@
+@file:Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "Chainy"
