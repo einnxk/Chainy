94aada854140503a30dc365d3b42f21933e19e97
EinNxk <einnxk@gmail.com>
2026-08-11 00:54:08 +0200

    Introduced callback events (#2)
    * feature: Introduced first callback logic
    
    * feat: Introduced callbacks
    
    * ref: refactored to spotless

---
+++ src/main/java/dev/einnik/chainy/callback/StepFailureCallback.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.einnik.chainy.callback;
+
+import org.jspecify.annotations.NonNull;
+
+/**
+ * The {@link Callback} interface that allows the api consumer to listen when a step execution fails
+ * and throws an {@link Exception}.
+ *
+ * @author EinNik
+ * @since 1.0.0
+ */
+@FunctionalInterface
+public interface StepFailureCallback extends Callback {
+  void callback(int stepCount, @NonNull Exception exception);
+}
