7bea4117681bdcfa3533651376ce2c945d8e7f46
EinNxk <einnxk@gmail.com>
2026-08-11 00:53:27 +0200

    ref: refactored to spotless

--- src/main/java/dev/einnik/chainy/callback/StepSuccessCallback.java
+++ src/main/java/dev/einnik/chainy/callback/StepSuccessCallback.java
@@ -1,20 +1,33 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.einnik.chainy.callback;
 
+import java.time.Duration;
 import org.jspecify.annotations.NonNull;
 
-import java.time.Duration;
-
 /**
- * The {@link Callback} interface that allows the api consumer
- * to listen when a new step has been completed successfully.
+ * The {@link Callback} interface that allows the api consumer to listen when a new step has been
+ * completed successfully.
  *
  * @param <T> the input that we invoke
  * @param <R> and the result the step produces
- *
  * @author EinNik
  * @since 1.0.0
  */
 @FunctionalInterface
 public interface StepSuccessCallback<T, R> extends Callback {
-    void callback(int stepCount, @NonNull T input, @NonNull R result, Duration duration);
+  void callback(int stepCount, @NonNull T input, @NonNull R result, Duration duration);
 }
