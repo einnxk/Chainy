ee23c64cfaaa690fec8b9b468203ed50f7e753a9
EinNxk <einnxk@gmail.com>
2026-08-11 01:27:46 +0200

    ref: refactored to spotless

--- src/main/java/dev/einnik/chainy/function/ChainyConsumer.java
+++ src/main/java/dev/einnik/chainy/function/ChainyConsumer.java
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.einnik.chainy.function;
 
 /**
- * A wrapped form of the {@link java.util.function.Consumer} functional interface, which
- * allows checked interfaces.
+ * A wrapped form of the {@link java.util.function.Consumer} functional interface, which allows
+ * checked interfaces.
  *
  * @param <T> the type of the parameter
  * @author EinNik
@@ -10,5 +25,5 @@ package dev.einnik.chainy.function;
  */
 @FunctionalInterface
 public interface ChainyConsumer<T> {
-    void accept(T input) throws Exception;
+  void accept(T input) throws Exception;
 }
